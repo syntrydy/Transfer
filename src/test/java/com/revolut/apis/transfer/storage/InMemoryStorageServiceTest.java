c443a726a196d2270f3f85365d3b764fdae34321
Gasmyr <gasmyrmougang@yahoo.fr>
2019-09-01 18:04:02 +0100

    Fix compilation error

--- src/test/java/com/revolut/apis/transfer/storage/InMemoryStorageServiceTest.java
+++ src/test/java/com/revolut/apis/transfer/storage/InMemoryStorageServiceTest.java
@@ -1,24 +1,15 @@
 package com.revolut.apis.transfer.storage;
 
 
-import com.revolut.apis.transfer.storage.data.StorageProvider;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(StorageProvider.class)
 public class InMemoryStorageServiceTest {
 
   private InMemoryStorageService inMemoryStorageService;
 
   @Before
   public void setUp(){
-    PowerMockito.mockStatic(StorageProvider.class);
     inMemoryStorageService = new InMemoryStorageService();
   }
 
@@ -27,6 +18,7 @@ public class InMemoryStorageServiceTest {
 
     inMemoryStorageService.getTransactions();
 
-    Mockito.when(StorageProvider.getTransactions()).thenCallRealMethod();
+    //TODO
+
   }
 }
