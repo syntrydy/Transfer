23eb4d00ca47bedfd9e27f1182a0b70f867e3736
Hendrick <osseux.hendrick@gmail.com>
2020-12-23 11:36:48 +0100

    Addition

---
+++ src/main/java/com/revolut/apis/transfer/Calculatrice.java
@@ -0,0 +1,27 @@
+package com.revolut.apis.transfer;
+
+public class Calculatrice {
+
+	private String name = "Calculatrice";
+
+	public int addition(int nombre1, int nombre2) {
+		return nombre1 + nombre2;
+	}
+
+	public int soustraction(int nombre1, int nombre2) {
+		return nombre1 - nombre2;
+	}
+
+	public int multiplication(int nombre1, int nombre2) {
+		return nombre1 * nombre2;
+	}
+
+	public double division(int nombre1, int nombre2) {
+		return nombre1 / nombre2;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
---
+++ src/main/java/com/revolut/apis/transfer/Main.java
@@ -0,0 +1,16 @@
+package com.revolut.apis.transfer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int nombre1 = 6;
+		int nombre2 = 3;
+		Calculatrice calculatrice = new Calculatrice();
+
+		System.out.println(
+				"Résultat addition de " + nombre1 + " + " + nombre2 + " = " + calculatrice.addition(nombre1, nombre2));
+
+	}
+
+}
